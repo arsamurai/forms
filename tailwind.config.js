56e44a3a5f2dde6af6c51b9384d56a50c45e6af9
Artur Musienko <musienkoartur45@gmail.com>
2024-07-26 11:38:11 +0300

    husky

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		container: {
-			center: true,
-			padding: '1rem',
-			screens: {
-				lg: '1200',
-			},
-		},
-		extend: {},
-	},
-	plugins: [],
-};
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1200",
+      },
+    },
+    colors: {
+      white: "rgba(255, 255, 255, 1)",
+      black: "rgba(0, 0, 0, 1)",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
