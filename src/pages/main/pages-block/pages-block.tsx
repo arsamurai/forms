7d0569c38f3114ac118cba8f74a63b40eae9c2fb
Artur Musienko <musienkoartur45@gmail.com>
2024-08-28 13:44:50 +0300

    fix main-page links

--- src/pages/main/pages-block/pages-block.tsx
+++ src/pages/main/pages-block/pages-block.tsx
@@ -6,13 +6,13 @@ import { pageLinks } from "./page-links.constants"
 
 const PagesBlock = () => {
   return (
-    <div className="flex h-fit min-h-[1024px] w-full max-w-[1440px] gap-5 bg-white px-[70px] py-[136px]">
+    <div className="grid h-fit w-full max-w-[1440px] grid-cols-1 gap-5 bg-white p-10 sm:grid-cols-2 md:grid-cols-3 md:px-[70px] md:py-[136px]">
       {pageLinks.map(link => (
         <Link
           key={link.href}
           to={link.href}
           className={cn(
-            "flex h-40 w-[310px] items-center justify-center rounded-[20px] text-2xl",
+            "flex h-40 w-full items-center justify-center rounded-[20px] p-2 text-center text-2xl",
             link.color,
           )}
         >
